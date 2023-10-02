8b3c87335e5b71f7cf771c6158938dce741aa814
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-10-02 14:19:37 +0100

    added PlaylistController

--- playlist-api/src/main/java/com/example/playlist/PlaylistController.java
+++ playlist-api/src/main/java/com/example/playlist/PlaylistController.java
@@ -1,6 +1,7 @@
 package com.example.playlist;
 
-import com.example.playlist.models.Bands;
+import com.example.playlist.models.Band;
+import com.example.playlist.models.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,20 @@ import java.util.List;
 @RestController
 public class PlaylistController {
 
-//  @Autowired
-//  PlaylistService playlistService;
+  @Autowired
+  PlaylistService playlistService;
 
   @GetMapping("/")
   public String greet() {
-    return "Welcome!";
+    return "Welcome to the Jungle!";
   }
+
+  @GetMapping("/bands")
+  public List<Band> getAllBands() { return playlistService.getAllBands();}
+
+  @GetMapping("/bands/id")
+  public Band getBandById(long id) { return playlistService.getBandById(id);}
+
+  @GetMapping("/bands/tracks")
+  public List<Track> getTracksByBandId() { return playlistService.getAllTracks();}
 }
